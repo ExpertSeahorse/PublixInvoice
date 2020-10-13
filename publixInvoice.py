84eb6fc38fd23a76661d169385c22a9733d7a366
ExpertSeahorse <dtfeldman@verizon.net>
2020-10-13 01:13:42 -0400

    Finalized publixInvoice

--- publixInvoice.py
+++ publixInvoice.py
@@ -3,10 +3,10 @@ import sys
 import re
 
 from General_Packages import read_my_email
-import Venmo
 
 
 # For each subject...
+ticket_arr = []
 subjects = ["FW: Your Publix receipt.", 'Fwd: Your Publix receipt.']
 for s in subjects:
     # Look thru my inbox for matches and...
@@ -16,11 +16,17 @@ for s in subjects:
         total = re.findall(r'Grand Total\s+\d+\.\d+', mail)
         date = re.findall(r'\d{2}\/\d{2}\/\d{4}', mail)
         print(total, date)
+        ticket_arr.append(total, date)
 
-        # Send venmo request to Kristen
+# Send any venmo request to Kristen
+if ticket_arr:
+    import Venmo
+    for total, date in ticket_arr:
         total = float(re.search(r"\d+.\d+", total[0]).group()) # Get the number out of the string    
         message = "Publix " + date
         Venmo.charge_money(total/2, 'Kristen-Lockhart-10', message)
-    
+            
+    Venmo.logout()
 
-Venmo.logout()
+else:
+    print("No receipts today.")
